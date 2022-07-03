342679ed37e414887bc61b3d71658d72d53dc81d
gavr <gavr123456789@gmail.com>
2022-07-03 18:37:37 +0300

    add decimal literals support

--- niva.nim
+++ niva.nim
@@ -1,14 +1,5 @@
 import "nivaPrelude"
 {. push discardable .}
 {. push warning[ProveField]:on .}
-type Person = object
-  name: string
-
-proc construct_Person_bob(): Person =
-  Person(name: "Bob")
-proc construct_Person_fromName(name: auto): auto =
-  Person(name: name)
-var y: Person = construct_Person_bob()
-var x: Person = construct_Person_fromName("qwe")
-y.name.`echo`()
-x.name.`echo`()
+var x = 7.9
+x.`echo`()
