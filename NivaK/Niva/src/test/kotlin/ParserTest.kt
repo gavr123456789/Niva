0bd751f3ccbd3f9f7608178c939bc34588f3dd24
gavr123456789 <gavr123456789@gmail.com>
2023-05-14 14:34:44 +0300

    2 declarations

--- NivaK/Niva/src/test/kotlin/ParserTest.kt
+++ NivaK/Niva/src/test/kotlin/ParserTest.kt
@@ -1,16 +1,11 @@
-import frontend.parser.Parser
-import frontend.parser.UnaryMsg
-import frontend.parser.VarDeclaration
-import frontend.parser.parse
+import frontend.parser.*
 import org.testng.annotations.Test
 
 class ParserTest {
     @Test
     fun varDeclaration() {
         val source = "x::int = 1"
-        val tokens = lex(source)
-        val parser = Parser(file = "", tokens = tokens, source = "sas.niva")
-        val ast = parser.parse()
+        val ast = declarationList(source)
         println("ast.count = ${ast.count()}")
         assert(ast.count() == 1)
         println("ast = $ast")
@@ -25,9 +20,7 @@ class ParserTest {
     @Test
     fun varDeclWithTypeInfer() {
         val source = "x = 1"
-        val tokens = lex(source)
-        val parser = Parser(file = "", tokens = tokens, source = "sas.niva")
-        val ast = parser.parse()
+        val ast = declarationList(source)
         assert(ast.count() == 1)
 
         val declaration: VarDeclaration = ast[0] as VarDeclaration
@@ -39,14 +32,11 @@ class ParserTest {
     @Test
     fun string() {
         val source = "\"sas\""
-        val tokens = lex(source)
-        val parser = Parser(file = "", tokens = tokens, source = "sas.niva")
-        val ast = parser.parse()
+        val ast = declarationList(source)
         assert(ast.count() == 1)
 
         val declaration = ast[0] as UnaryMsg
-//        println("declaration.type = ${declaration.type}")
-//        assert(declaration.type == "string")
+
         assert(declaration.str == "\"sas\"")
     }
 
@@ -54,17 +44,43 @@ class ParserTest {
     @Test
     fun helloWorld() {
         val source = "\"sas\" echo"
+        val ast = declarationList(source)
+        assert(ast.count() == 1)
+
+        val unaryMsg = ast[0] as UnaryMsg
+
+        assert(unaryMsg.selectorName == "echo")
+        assert(unaryMsg.receiver.type == "string")
+        assert(unaryMsg.receiver.str == "\"sas\"")
+    }
+
+
+    @Test
+    fun varDeclAndUnaryMsg() {
+        val source = """
+        x = 1
+        x echo
+    """.trimIndent()
+        val ast = declarationList(source)
+        assert(ast.count() == 2)
+
+        val declaration = ast[0] as VarDeclaration
+        val unaryMsg = ast[1] as UnaryMsg
+        assert(declaration.name.str == "x")
+        assert(declaration.value.type == "int")
+        assert(declaration.value.str == "1")
+
+        assert(unaryMsg.selectorName == "echo")
+//        assert(unaryMsg.receiver.type == "string")
+        assert(unaryMsg.receiver.str == "x")
+
+
+    }
+
+    private fun declarationList(source: String): List<Declaration> {
         val tokens = lex(source)
         val parser = Parser(file = "", tokens = tokens, source = "sas.niva")
         val ast = parser.parse()
-        assert(ast.count() == 1)
-
-        val declaration = ast[0]
-        assert(declaration is UnaryMsg)
-        val decl = declaration as UnaryMsg
-
-        assert(decl.selectorName == "echo")
-        assert(decl.receiver.type == "string")
-        assert(decl.receiver.str == "\"sas\"")
+        return ast
     }
 }
