b012472387daa3cacd609a56275519f5ad59f3d9
gavr123456789 <gavr123456789@gmail.com>
2024-02-17 03:11:46 +0200

    [infro] native-image build plugin

---
+++ Niva/Niva/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    // ...
+
+    // Apply GraalVM Native Image plugin
+    id("org.graalvm.buildtools.native") version "0.10.0"
+}
+
+
+
+graalvmNative {
+    binaries {
+//        named("main") {
+//            imageName.set("niva")
+//            buildArgs.add("-O4")
+//        }
+
+    }
+    binaries.all {
+        imageName.set("niva")
+        buildArgs.add("-O4")
+        buildArgs.add("--static")
+        buildArgs.add("--no-fallback")
+        buildArgs.add("-march=native")
+        buildArgs.add("--initialize-at-build-time")
+    }
+}
