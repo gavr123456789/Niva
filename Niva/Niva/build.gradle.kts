d353cc996ec1d7d34344af9602f7915b55371843
gavr123456789 <gavr123456789@gmail.com>
2024-02-19 03:00:21 +0200

    [info] remove graalvm plugin since it broke the jvmRun task rerun

--- Niva/Niva/build.gradle.kts
+++ Niva/Niva/build.gradle.kts
@@ -1,23 +1,16 @@
-plugins {
-    id("org.graalvm.buildtools.native") version "0.10.0"
-}
-
-
-
-graalvmNative {
-    binaries {
-//        named("main") {
-//            imageName.set("niva")
-//            buildArgs.add("-O4")
-//        }
-
-    }
-    binaries.all {
-        imageName.set("niva")
-        buildArgs.add("-O4")
-        buildArgs.add("--static")
-        buildArgs.add("--no-fallback")
-        buildArgs.add("-march=native")
-        buildArgs.add("--initialize-at-build-time")
-    }
-}
+//plugins {
+//    id("org.graalvm.buildtools.native") version "0.10.0"
+//}
+//
+//
+//
+//graalvmNative {
+//    binaries.all {
+//        imageName.set("niva")
+//        buildArgs.add("-O4")
+//        buildArgs.add("--static")
+//        buildArgs.add("--no-fallback")
+//        buildArgs.add("-march=native")
+//        buildArgs.add("--initialize-at-build-time")
+//    }
+//}
