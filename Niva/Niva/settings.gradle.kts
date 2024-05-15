49b8110bbdceb3b96b687e544b4144e63671417c
gavr123456789 <gavr123456789@gmail.com>
2024-05-15 04:26:21 +0300

    [build] add foojay

--- Niva/Niva/settings.gradle.kts
+++ Niva/Niva/settings.gradle.kts
@@ -9,9 +9,10 @@ pluginManagement {
     }
 }
 
-//plugins {
+plugins {
 //    id("org.jetbrains.amper.settings.plugin").version("0.2.0")
-//}
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
 rootProject.name = "niva"
 
