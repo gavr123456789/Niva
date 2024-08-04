b93a487db4fb3be126c9029c2aefa7b191c2e89b
gavr <gavr123456789@gmail.com>
2024-08-04 05:44:52 +0300

    [build] add icon and more gradle tasks

--- Niva/Niva/build.gradle.kts
+++ Niva/Niva/build.gradle.kts
@@ -1,3 +1,10 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+import java.io.ByteArrayOutputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+
+
 plugins {
     kotlin("jvm") version "2.0.0"
     application
@@ -22,6 +29,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
@@ -37,7 +45,14 @@ graalvmNative {
 
         imageName.set("niva")
         buildArgs.add("-O4")
-        buildArgs.add("--static")
+
+        if (DefaultNativePlatform.getCurrentOperatingSystem().isLinux) {
+            println("You are using linux! binary will be static")
+            buildArgs.add("--static")
+        } else {
+            println("No linux = no static binary for u")
+        }
+
         buildArgs.add("--no-fallback")
         buildArgs.add("-march=native")
         buildArgs.add("--initialize-at-build-time")
@@ -48,6 +63,85 @@ application {
     mainClass = "main.MainKt"
 }
 
+
+val checkAndBuildNativeTask = "checkAndBuildNative"
+val buildNativeNiva = "buildNativeNiva"
+val javaHomeMayBeHere = "Library/Java/JavaVirtualMachines/graalvm-jdk-22.0.2/Contents/Home"
+
+val checkGraalVMTask = "checkGraalVM"
+
+tasks.register(checkGraalVMTask) {
+    doFirst {
+        println("Checking GraalVM...")
+        val javaVersionOutput = ByteArrayOutputStream()
+        exec {
+            commandLine("java", "-version")
+            standardOutput = javaVersionOutput
+            errorOutput = javaVersionOutput
+            isIgnoreExitValue = true
+        }
+        val output = javaVersionOutput.toString()
+        if (!output.contains("GraalVM")) {
+            throw GradleException("\tCurrent Java is not GraalVM. Please set JAVA_HOME to GraalVM installation.\n" +
+                    "\tFor mac and linux: java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home'\n" +
+                    "\tFor windows: java -XshowSettings:properties -version 2>&1 | findstr \"java.home\"\n" +
+                    "\tOn mac its probably here: $javaHomeMayBeHere\n\n" +
+                    "\tThen set it with: \n" +
+                    "\t\tJAVA_HOME=value in bash-like shell \n" +
+                    "\t\tset JAVA_HOME value in fish shell\n" +
+                    "\t\twhere value is  \"java.home = THIS\" from the output of the previous command\n" +
+                    "\tand run `./gradlew buildNativeNiva` again")
+        } else {
+            println("GraalVM is found")
+        }
+    }
+}
+
+tasks.register(checkAndBuildNativeTask) {
+    dependsOn(checkGraalVMTask, "nativeCompile")
+}
+
+tasks.register(buildNativeNiva) {
+
+    dependsOn("publishToMavenLocal", checkAndBuildNativeTask)
+    doLast {
+        moveInfroDir()
+        moveBinary()
+    }
+}
+
+fun moveBinary() {
+    val userHome = System.getProperty("user.home")
+    val nivaBinary = file("${layout.buildDirectory.get()}/native/nativeCompile/niva").toPath()
+    val targetDir = file("$userHome/.niva/bin").toPath()
+    Files.createDirectories(targetDir)
+    copyRecursively(nivaBinary, targetDir)
+}
+
+fun moveInfroDir() {
+    val userHome = System.getProperty("user.home")
+
+    val sourceDir = file("${layout.projectDirectory}/../infroProject")
+    val targetDir = file("$userHome/.niva/infroProject")
+    val nivaDir = file("$userHome/.niva")
+
+
+
+    if (sourceDir.exists()) {
+        if (nivaDir.exists()) {
+            nivaDir.deleteRecursively()
+            println("$nivaDir deleted")
+        }
+
+        Files.createDirectories(nivaDir.toPath())
+        copyRecursively(sourceDir.toPath(), targetDir.toPath())
+        println("$userHome/.niva created")
+    } else {
+        println("Can't find: $sourceDir, please run $buildNativeNiva from Niva/Niva/Niva dir of the repo")
+    }
+}
+
+/////////
 
 
 publishing {
@@ -62,6 +156,19 @@ publishing {
     }
 }
 
+
+fun copyRecursively(source: Path, target: Path) {
+    Files.walk(source).forEach { path ->
+        val targetPath = target.resolve(source.relativize(path).toString())
+        if (Files.isDirectory(path)) {
+            if (!Files.exists(targetPath)) {
+                Files.createDirectory(targetPath)
+            }
+        } else {
+            Files.copy(path, targetPath, StandardCopyOption.REPLACE_EXISTING)
+        }
+    }
+}
 
 //////OLD
 //plugins {
