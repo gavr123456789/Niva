d26aab298fa34c25fb16b1e53144885a02ad53d9
Seggan <seggan21@gmail.com>
2024-12-09 17:01:07 -0500

    Add rename for types (#325)
    [codegen] rename pragma works for types fix #66

--- Niva/Niva/src/main/kotlin/codogen/GenerateType.kt
+++ Niva/Niva/src/main/kotlin/codogen/GenerateType.kt
@@ -4,12 +4,16 @@ import main.frontend.parser.types.ast.TypeAST
 import main.utils.isGeneric
 
 
-fun TypeAST.generateType(generateGeneric: Boolean = true, customGenerics: Set<String>? = null): String = buildString {
+fun TypeAST.generateType(
+    realName: String?,
+    generateGeneric: Boolean = true,
+    customGenerics: Set<String>? = null
+): String = buildString {
 //    val x: (String, Int) -> Int = {}
      when (this@generateType) {
         is TypeAST.InternalType -> append(name)
         is TypeAST.UserType -> {
-            append(names.joinToString("."))
+            append(realName ?: names.joinToString("."))
             if (generateGeneric && typeArgumentList.isNotEmpty()) {
                 val (genericSingleLetters, genericNames) =
                     typeArgumentList.asSequence().map { it.name }.partition { it.isGeneric() }
@@ -23,18 +27,18 @@ fun TypeAST.generateType(generateGeneric: Boolean = true, customGenerics: Set<St
         is TypeAST.Lambda -> {
             val realArgs = if (extensionOfType != null) {
                 // fun sas(x: ^Int.(Int) -> String) =
-                val kotlinExtType = extensionOfType.generateType()
+                val kotlinExtType = extensionOfType.generateType(realName)
                 append(kotlinExtType, ".")
                 inputTypesList.drop(1)
             } else inputTypesList
 
             append("(")
             realArgs.forEach {
-                append(it.generateType(), ",")
+                append(it.generateType(realName), ",")
             }
             append(") -> ")
 
-            append(returnType.generateType())
+            append(returnType.generateType(realName))
         }
     }
 
