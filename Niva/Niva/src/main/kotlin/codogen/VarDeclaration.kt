79a62ec6ef3756eccdbb97294a2e5098e783358e
gavr123456789 <gavr123456789@gmail.com>
2025-08-12 00:55:23 +0300

    [codegen nin] use astType for explicit typy annotation instead of real type, nin - switch typing

--- Niva/Niva/src/main/kotlin/codogen/VarDeclaration.kt
+++ Niva/Niva/src/main/kotlin/codogen/VarDeclaration.kt
@@ -10,11 +10,11 @@ fun VarDeclaration.generateVarStatement(): String {
     val valueCode = value.generateExpression()
 
     val valOrVar = if (!this.mutable) "val" else "var"
-    val valueTypeAst = valueTypeAst
+    val valueTypeAst = this.declaredType
     val valueType = value.type
     val pkgOfType = if (valueType is Type.UserLike && valueType.isBinding) (valueType.pkg + ".") else ""
     val type = if (valueTypeAst == null) "" else
-        ":$pkgOfType${valueType!!.toKotlinString(false)}"
+        ":$pkgOfType${valueTypeAst.toKotlinString(false)}"
     //"\n//@ ", tok.file.name, ":::", tok.line, "\n"
     val debugIfNeeded = if (GlobalVariables.needStackTrace)
         "\n//@ ${token.file.name}:::${token.line}\n"
