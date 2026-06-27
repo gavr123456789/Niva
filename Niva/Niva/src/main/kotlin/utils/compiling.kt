98cf9ca5764a3fde288381b5b75229b8401c3610
gavr123456789 <gavr123456789@gmail.com>
2026-06-27 06:36:17 +0300

    [nin] {a: name b: age} = person, syntax destruction

--- Niva/Niva/src/main/kotlin/utils/compiling.kt
+++ Niva/Niva/src/main/kotlin/utils/compiling.kt
@@ -459,7 +459,7 @@ fun listFilesDownUntilNivaIsFoundRecursively(directory: File, ext: String): Muta
     val filesAndDirs = directory.listFiles() ?: return mutableListOf()
 
     for (file in filesAndDirs) {
-        if (!file.name.startsWith("_")) {
+        if (!file.name.startsWith("_") && !file.name.startsWith(".")) {
             if (file.isFile && (file.extension == ext)) {
                 fileList.add(file)
             } else if (file.isDirectory) {
